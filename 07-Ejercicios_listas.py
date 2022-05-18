737f127f4f6e3ef6f5bfa7b1556ab8414fc5e9a5
Your Name <you@example.com>
2022-05-18 19:42:21 +0200

    Cadenas

--- 07-Ejercicios_listas.py
+++ 07-Ejercicios_listas.py
@@ -1,5 +1,6 @@
 thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
 
+'''
 Mostrar por consola:
 Mostrar banana
 Eliminar mango
@@ -9,4 +10,5 @@ Añadir pear al final
 Añadir mango en el mismo indice en el que estaba antes
 Desde el último elemento hasta el elemento del indice 4 (de atras a delante)
 Crear una lista nueva con distintos tipos de elementos y operar entre ella y thislist
+'''
 
---
+++ Curso/Strings/strings.py
@@ -0,0 +1,74 @@
+'''
+Strings literales o como escapar caracteres
+'''
+cadena = "Esto es un ejemplo de cadena de texto"
+print (cadena)
+cadena = "Esto es un \"ejemplo\" de cadena de texto"
+print (cadena)
+cadena = "Esto es un \nejemplo de cadena de texto"
+print (cadena)
+cadena = "Esto es un \tejemplo de cadena de texto"
+print (cadena)
+cadena = "Esto es un \bejemplo de cadena de texto"
+print (cadena)
+cadena = "Esto es un \fejemplo de cadena de texto"
+print (cadena)
+cadena = "Esto es un \rejemplo de cadena de texto"
+print (cadena)
+cadena = "Esto es un \vejemplo de cadena de texto"
+print (cadena)
+cadena = "Esto es un \aejemplo de cadena de texto"
+print (cadena)
+'''
+Convertir un número a string
+'''
+
+cadena1 = "Hola pibe"
+cadena2 = " o piba"
+numero = 1
+print(cadena1 + cadena2) # Concatenación o suma
+print(numero,  "Hola " , cadena2)
+
+print(cadena2 * 5) # Multiplicador
+
+'''
+Función STR
+'''
+
+print(type(numero))
+print(type(str(numero)))
+
+'''
+SubStrings
+Devanado de cadenas
+'''
+print(len(cadena))
+print(cadena)
+print(cadena[2]) # el primer caracter es el de orden 0
+print(cadena[0:10]) # Devanado
+print(cadena[:10])
+print(cadena[3:10])
+print(cadena[3:])
+print(cadena[-4: -1])
+
+'''
+Metodos de cadenas
+'''
+cadena = "    esto es un Ejemplo de cadena de texto Python@    " 
+cadena5 = "LazaroBelloch5"
+print(cadena.lower()) # Minúsculas
+print(cadena.upper()) # Mayúsculas
+print(cadena.capitalize()) #Primera letra en mayúsculas, solo la primera
+print(cadena.title()) # Primera letra de cada palabra en mayúsculas
+print(cadena.swapcase()) # Cambia minúsculas por mayúsculas y viceversa
+print(cadena.count("to")) # Suma la cantidad de to que hay en la cadena
+print(cadena.isalnum()) # Devuelve false si la cadena contiene carácteres no alfanuméricos
+print(cadena5.isalnum()) # Devuelve true si la cadena contiene solo carácteres alfanuméricos
+print(cadena.isalpha()) # Devuelve true si todos los carácteres son alfabeticos
+print(cadena.strip()) # Elimiena los espacios al principio y al final
+print(cadena5.zfill(20)) # Agrega ceros a la izquierda hasta completar el numero indicado de carácteres
+s = "; ".join(["1", "2", "3"])
+print(s)
+s = "Python,Java,C"
+print(s.split(",")) #['Python', 'Java', 'C']
+print(cadena.strip().split(" "))
